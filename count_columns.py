048f2807aa3db3314d80613d2cb28045c4725fa4
bhavyakarki <bkarki@andrew.cmu.edu>
2018-02-25 17:28:48 -0500

    Update count_columns.py

--- count_columns.py
+++ count_columns.py
@@ -1,10 +1,14 @@
+
 import os
 import pandas as pd
 
-
+print("hi")
 path = "./WL1"
 
 files = os.listdir(path)
-print(len(files))
-for current in files:
-	print current
+#print(len(files))
+for i,current in enumerate(files):
+	print str(i)+"= ",current
+	df = pd.read_table(current, sep='\t', low_memory=False)
+
+	print len(df.columns)
